777becabc5c962738d1ecea1c49c126631079d2f
bestofoleg <olegabramov1998@gmail.com>
2025-11-21 18:20:05 +0400

    убрал лишнее

--- src/main.rs
+++ src/main.rs
@@ -341,22 +341,6 @@ const WIN_PATTERNS: [[(usize, usize); 3]; 8] = [
     [(0,2), (1,1), (2,0)],
 ];
 
-// fn check_winner(game_field: Mut<GameField>) -> Option<PlayableCellState> {
-//     for &[a, b, c] in &WIN_PATTERNS {
-//         let (ax, ay) = a;
-//         let (bx, by) = b;
-//         let (cx, cy) = c;
-//
-//         if game_field.field[ax][ay] != PlayableCellState::Empty
-//             && game_field.field[ax][ay] == game_field.field[bx][by]
-//             && game_field.field[bx][by] == game_field.field[cx][cy] {
-//             return Some(game_field.field[ax][ay]);
-//         }
-//     }
-//
-//     None
-// }
-
 fn check_winner(game_field: &GameField) -> Option<PlayableCellState> {
     // Проверка победы
     for &[a, b, c] in &WIN_PATTERNS {
